beccd96fe9ee8716626bebfc256d3d8eebb8ec51
Sammy Hajhamid <sh@pepsi.pw>
2026-01-28 01:28:52 -0800

    clean up css

--- src/components/card.rs
+++ src/components/card.rs
@@ -4,16 +4,18 @@ use super::motd;
 
 pub fn server_card(name: &str, state: &str, motd_text: &str, icon_url: &str) -> Markup {
     html! {
-        .card {
-            img.server-icon
-                src=(icon_url)
-                width="64"
-                height="64"
-                alt="" {}
-            .card-body {
-                div { (name) }
+        .card.server-card {
+            .server-card-header {
+                strong { (name) }
+                span { (state) }
+            }
+            .server-card-content {
+                img.server-icon
+                    src=(icon_url)
+                    width="64"
+                    height="64"
+                    alt="" {}
                 div class="mc-motd" { (motd::render_motd(motd_text)) }
-                div { (state) }
             }
         }
     }
